3b7df6a4de452567512bcae4a1c1fc6d5347bd41
Yaşar Arabacı <yasar11732@gmail.com>
2024-09-26 13:03:22 +0300

    Fix regression

--- src/parse_args.h
+++ src/parse_args.h
@@ -24,7 +24,7 @@ static bool parse_args(fed *f,int argc, char *argv[])
         } else if(streq("-t", argv[i]) && (i+1 < argc)) {
             i++;
             long t = strtol(argv[i], (char**)NULL, 10);
-            if(t > 0 && t < LONG_MAX && t <= INT_MAX) { // a signed int is guaranteed to this big  
+            if(t > 0 && t < LONG_MAX && t <= INT_MAX) {
                 f->numListed = (int)t;
             } else {
                 success = false;
